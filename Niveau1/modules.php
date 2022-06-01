7965ddc87fffcb65674ed92ead9d1ec4bbef6374
Bynood <quent1_jarry@hotmail.com>
2022-06-01 16:22:56 +0200

    déploiement du refacto de navbar sur toutes les pages

--- Niveau1/modules.php
+++ Niveau1/modules.php
@@ -24,17 +24,3 @@
     }
 ?>
 
-<!-- TEST CODE -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php echo navbar_link("https://www.google.com", "MonLien") ?>
-</body>
-</html>
-
