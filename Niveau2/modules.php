47d577404bdaef4d55a96e411a4e3c412983faeb
Bynood <quent1_jarry@hotmail.com>
2022-06-03 10:04:24 +0200

    ajout de variable de connexion BDD dans modules.php

--- Niveau2/modules.php
+++ Niveau2/modules.php
@@ -1,26 +1,32 @@
 <?php 
- $navbar = '
- <nav id="menu">
-    <a href="news.php">Actualités</a>
-    <a href="wall.php?user_id=5">Mur</a>
-    <a href="feed.php?user_id=5">Flux</a>
-    <a href="tags.php?tag_id=1">Mots-clés</a>
-</nav>
-<nav id="user">
-    <a href="#">Profil</a>
-    <ul>
-        <li><a href="settings.php?user_id=5">Paramètres</a></li>
-        <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-        <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-    </ul>
+    $navbarfix = '
+        <nav id="menu">
+            <a href="news.php">Actualités</a>
+            <a href="wall.php?user_id=5">Mur</a>
+            <a href="feed.php?user_id=5">Flux</a>
+            <a href="tags.php?tag_id=1">Mots-clés</a>
+        </nav>
+        <nav id="user">
+            <a href="#">Profil</a>
+            <ul>
+                <li><a href="settings.php?user_id=5">Paramètres</a></li>
+                <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
+                <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+            </ul>
 
-</nav>'
-?>
+        </nav>
+        ';
 
-<?php 
+    $navbar = $navbarfix;    
+
+    $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+
     function navbar_link($link, $name){
         $output = "<a href=$link>$name</a>";
         return $output;
-    }
+    };
+
+    
+
 ?>
 
